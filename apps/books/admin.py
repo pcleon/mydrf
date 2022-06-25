d517493a319826a3ae40f006220f65d00861841e
pcleon <wpctszz@qq.com>
2022-06-25 23:54:24 +0800

    dbops:初始化

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -8,9 +8,11 @@ class BookInfoAdm(admin.ModelAdmin):
     list_display = ['id', 'name', 'create_time', 'read_numbers', 'comment']
     list_display_links = ['id', 'name']
 
+
 class HeroInfoAdm(admin.ModelAdmin):
     list_display = ['id', 'name', 'gender', 'comment', 'book']
     list_display_links = ['id', 'name', 'book']
+
 
 admin.site.register(BookInfo, BookInfoAdm)
 admin.site.register(HeroInfo, HeroInfoAdm)
